f88c33809e922cd315be54e1a801ec05e2a780b3
PEDRO MARINHO RODRIGUES CAMARGO CORRÊA <pedro.correa45@etec.sp.gov.br>
2025-05-07 10:18:17 -0300

    first commit

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, Button, StyleSheet,Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function DetailsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>Details Screen</Text>
+        <View style={styles.buttonContainer}> 
+            <Button
+                title="Go Back"
+                onPress={() => navigation.goBack("Home")}
+            />
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f0f8ff",
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
+    buttonContainer: {
+        backgroundColor: "#add8e6",
+        width: windowWidth * 0.5,
+        margin: 10,
+        borderRadius: 5,
+    },
+});
